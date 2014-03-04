becfe775d7ba92b3edcda2167ce3e65d4fe04857
Michael Vezzani <mjvezzani@gmail.com>
2014-03-04 08:55:32 -0500

    Working proof of concept for ckeditor. Run rake db:migrate, rails s, then /posts/new to see editor in action.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CkeditPoc::Application.config.secret_key_base = '24df200dd7fc92c80740b17900a3a4fc75c3bd2b0e10d53962924a3b4501bb0171e4d200df15a113ddd21c4fa0f1c380c5244cc9646191526df8b55fe43286d6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CkeditPoc::Application.config.session_store :cookie_store, key: '_ckedit_poc_session'
